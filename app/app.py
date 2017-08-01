adf5fc5756ed3f99ace31f5906df448d8bdc22af
Bianca Rosa <me@biancarosa.com.br>
2017-07-31 23:23:27 -0300

    versiona health check

--- app/app.py
+++ app/app.py
@@ -3,6 +3,6 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/health-check')
+@app.route('/v1/health-check')
 def health_check():
     return jsonify({"message" : "I'm okay"}), 200
